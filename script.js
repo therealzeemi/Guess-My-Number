e49c4aae9552f554fcbd2aa1859d926f60d66933
Azeem Sajid <147242891+therealzeemi@users.noreply.github.com>
2025-02-18 14:38:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+let displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+let showScore = function (score) {
+  document.querySelector('.score').textContent = score;
+};
+
+// Check Button
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    displayMessage('⛔ No number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct number!');
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      showScore(score);
+    } else {
+      displayMessage('💥 You lost the game!');
+      showScore(0);
+    }
+  }
+});
+
+// Again Button
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  document.querySelector('.number').textContent = '?';
+  displayMessage('Start guessing...');
+  showScore(score);
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
